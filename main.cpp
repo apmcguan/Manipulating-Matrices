ba06c05422c965914613057dcc84e754ecec9447
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-10-19 15:50:52 +0000

    3 collum

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
   int AirQuality_Index[N_ROWS][N_COLS] = {
   {50, 68, 58}, {58, 115, 78}, {62, 110, 81}, {66, 124, 110}, {64, 152, 121}, {46, 143, 90}, {46, 143, 80},{25, 91, 91}, {27, 19, 59}, {17, 13, 27}
   };
- cout<<setw(2)<<"Grand Junction,Co"<<setw(6)<<" Fort Collins"<<setw(12)<<"    Denver- Boulder"<<endl;
+ cout<<setw(2)<<"  Grand Junction"<<setw(6)<<"  Fort Collins"<<setw(12)<<"  Denver- Boulder"<<endl;
   
 //  cout<<"My matrix;"<<endl;
   printMatrix_3col(AirQuality_Index, N_ROWS, N_COLS);
@@ -25,7 +25,7 @@ void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS)
     for (row = 0; row < N_ROWS; row++)
     {
         for(col = 0; col < N_COLUMNS; col++)
-            cout << setw(10) << matrix[row][col] << " ";
+            cout << setw(12) << matrix[row][col] << " ";
         
         cout << endl;    
     }
